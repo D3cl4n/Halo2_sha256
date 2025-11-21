ccd951f726d6c7ed77448f7a653566ecda8c722b
D3cl4n <dmurphyp546@gmail.com>
2025-11-21 18:36:51 -0500

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::marker::PhantomData;
+use ff::Field;
+use halo2_proofs::{
+    circuit::{AssignedCell, Chip, Layouter, Region, SimpleFloorPlanner, Value},
+    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Fixed, Instance, Selector},
+    poly::Rotation
+};
+
+
 fn main() {
     println!("Hello, world!");
 }
