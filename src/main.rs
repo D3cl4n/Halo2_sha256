332b047dc720489139bf125d252c644f3a8c544b
D3cl4n <dmurphyp546@gmail.com>
2025-11-23 11:39:47 -0500

    working on gates and constraints

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ struct Sha256Circuit<F: Field> {
 #[derive(Clone, Debug)]
 struct Sha256Config {
     advice: [Column<Advice>; 8], // one advice column per internal variable h0, h1, ..., h7
+    fixed: Column<Fixed>, // fixed column for the sha256 round constants
     instance: Column<Instance>,
     s_add_mod32: Selector,
     s_rotate: Selector,
@@ -51,7 +52,7 @@ impl<F: Field> Chip<F> for Sha256Chip<F> {
     }
 }
 
-// trait for the Sha256Chip
+// trait for the Sha256Chip bitwise operations
 trait Sha256Operations<F: Field>: Chip<F> {
     type Num;
 
@@ -78,6 +79,54 @@ trait Sha256Operations<F: Field>: Chip<F> {
 
     // bitwise shift on a given number by n positions. Shift left if flag is true, right if false
     fn bitwise_shift(&self, layouter: impl Layouter<F>, a: Self::Num, n: usize, direction: bool) -> Result<Self::Num, Error>;
+}
+
+// implementing gates and supporting methods for the Sha256Chip
+impl<F: Field> Sha256Chip<F> {
+    // constructor
+    fn construct(config: <Self as Chip<F>>::Config) -> Self {
+        Sha256Chip { config, _marker: PhantomData, }
+    }
+
+    // configure the chip with the set of columns, gates, and constraints
+    fn configure(
+        meta: &mut ConstraintSystem<F>,
+        advice: [Column<Advice>; 8],
+        fixed: Column<Fixed>,
+        instance: Column<Instance>
+    ) -> <Self as Sha256Chip<F>>::Config {
+        meta.enable_equality(instance); // enable the copy constraint
+        meta.enable_constant(fixed);
+
+        // enable copy constraint on each advice column
+        for advice_col in &advice {
+            meta.enable_equality(*advice_col);
+        }
+
+        // all the selectors the chip needs for the set of gates
+        let s_add_mod32 = meta.selector();
+        let s_rotate = meta.selector();
+        let s_xor = meta.selector();
+        let s_and = meta.selector();
+        let s_shift = meta.selector();
+
+        // define the set of gates and constraints for the chip
+        meta.create_gate("addition_mod32_gate", |meta| {
+            //TODO: figure out how to do addition mod 2^32 here
+        });
+
+        // return the Sha256Chip config
+        Sha256Config {
+            advice,
+            fixed, 
+            instance, 
+            s_add_mod32,
+            s_rotate,
+            s_xor,
+            s_and,
+            s_shift,
+        }
+    }
 }
 
 
